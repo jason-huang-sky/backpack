b6f9be774135baa63d93671f849cdba9a4d9e177
Jason Huang <jason.huang@skyscanner.net>
2018-05-21 16:03:35 +0800

    add marker on the map

--- packages/bpk-component-map/stories.js
+++ packages/bpk-component-map/stories.js
@@ -20,7 +20,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import BpkMap from './index';
+import { withRtlSupport } from 'bpk-component-icon';
+import HotelsLargeIcon from 'bpk-component-icon/lg/hotels';
+import HotelsSmallIcon from 'bpk-component-icon/sm/hotels';
+import BpkTooltip from 'bpk-component-tooltip';
+
+import { BpkMap, BpkMapMarker } from './index';
+
+const RtlSmallHotelsIcon = withRtlSupport(HotelsSmallIcon);
+const RtlLargeHotelsIcon = withRtlSupport(HotelsLargeIcon);
 
 const zoom = level => {
   console.info(level);
@@ -73,4 +81,18 @@ storiesOf('bpk-component-map', module)
       boundEast={-3.159632742578083}
       onDrag={drag}
     />
+  ))
+  .add('Add Markers on the map', () => (
+    <BpkMap apiKey="" centerLatitude={55.944357} centerLongitude={-3.1967116}>
+      <BpkMapMarker latitude={55.944357} longitude={-3.1967116}>
+        <BpkTooltip id="my-tooltip1" target={<RtlSmallHotelsIcon />}>
+          Jinglun Hotel
+        </BpkTooltip>
+      </BpkMapMarker>
+      <BpkMapMarker latitude={55.943763} longitude={-3.195272}>
+        <BpkTooltip id="my-tooltip2" target={<RtlLargeHotelsIcon />}>
+          The Langham London
+        </BpkTooltip>
+      </BpkMapMarker>
+    </BpkMap>
   ));
